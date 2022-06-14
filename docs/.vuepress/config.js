9720f64f2adbdbff879fc5589f34cb499af44fa6
DanielLeefu <you@2228429150@qq.com>
2022-06-14 22:12:05 +0800

    feat: init git repo

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  base: "/",
+  title: "前端部署实践指南",
+  description: "从基础到实践，带你全面了解和掌握前端部署流程",
+  head: [
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "Docker, CI, CD, jenkins, git Actions, 前端, 部署",
+      },
+    ],
+    ["meta", { name: "author", content: "九思" }],
+  ],
+  plugins: [
+    "@vuepress/medium-zoom",
+    "@vuepress/back-to-top",
+    "@vuepress/active-header-links",
+  ],
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    logo: "/images/logo.png",
+    repo: "https://github.com/DanielLeefu/DeploymentGuide",
+    nav: [
+      {
+        text: "Issue",
+        link: "https://github.com/DanielLeefu/DeploymentGuide/issues",
+      },
+      {
+        text: "更多",
+        link: ""
+      }
+    ],
+    sidebar: [
+      {
+        title: "介绍",
+        collapsable: false,
+        children: [
+          "/",
+        ],
+      },
+      {
+        title: "基础入门",
+        collapsable: false,
+        children: [
+          "/basic/git-actions/",
+        ],
+      },
+      {
+        title: "部署实践",
+        collapsable: false,
+        children: [
+         "/basic/deploy-simple/"
+        ],
+      },
+      {
+        title: "进阶指南",
+        collapsable: false,
+        children: [
+        
+        ],
+      },
+    ],
+
+    // 搜索
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: "最后更新",
+  },
+
+  // PWA 配置
+  serviceWorker: true,
+};
---
+++ docs/basic/deploy-simple/README.md
@@ -0,0 +1,22 @@
+
+# 部署单页面
+
+我们来通过部署一个自己的博客页面来进行实践, 使用[vuepress]() 来生成页面
+
+## 初始化
+
+先来对项目进行初始化，创建一个博客页面仓库，deploy_blog, 
+```
+npm init -y
+npm install -D vuepress
+```
+在package.json 中添加dev和build的scripts
+
+```
+"dev": "vuepress dev docs",
+"build": "vuepress build docs"
+
+```
+
+根据vuepress规范来创建文件夹，创建一个docs文件夹，然后创建.vuepress文件夹，在.vuepress 里面创建config.js,来写入配置文件
+
