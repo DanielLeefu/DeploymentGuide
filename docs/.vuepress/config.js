2e9ba510bf8ace96eb94ea237ee39fed19b8f6d6
DanielLeefu <you@2228429150@qq.com>
2022-06-14 23:09:45 +0800

    feat: add deploy main ci

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -47,6 +47,7 @@ module.exports = {
         collapsable: false,
         children: [
           "/basic/git-actions/",
+          "/basic/git-pages/",
         ],
       },
       {
@@ -60,7 +61,7 @@ module.exports = {
         title: "进阶指南",
         collapsable: false,
         children: [
-        
+          "/basic/git-actions-create/"
         ],
       },
     ],
